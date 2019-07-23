2337638b470f0a8d42667e40c1821e86a2892736
Roman Mytsko <roma.mytsko@icloud.com>
2019-07-23 08:43:23 +0300

    All code of working with DB moved to class and to other module

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 import os.path
 import psycopg2
+from work_with_db import Database_work
 
 
 class Calculator:
@@ -46,7 +47,7 @@ def check_input_num(number):
 
 def check_wish(wish):
     if wish == "y":
-        read_results(table_name)
+        work_with_base.read_results(connection, table_name)
         return False
     elif wish == "n":
         return False
@@ -55,86 +56,18 @@ def check_wish(wish):
         return True
 
 
-def check_table(table_name):
-    connection = psycopg2.connect(user="roma",
-                                  password="thesoprano777",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="calculator_db")
-    cursor = connection.cursor()
-    cursor.execute("select * from information_schema.tables where table_name=%s", (table_name,))
-    result_of_exist_table = bool(cursor.rowcount)
-    cursor.close()
-    connection.close()
-    return result_of_exist_table
-
-
-def create_table(table_name):
-    connection = psycopg2.connect(user="roma",
-                                  password="thesoprano777",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="calculator_db")
-    cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE %s
-                            (action_id SERIAL PRIMARY KEY,
-                             first_number REAL NOT NULL, 
-                             action TEXT NOT NULL,
-                             second_number REAL NOT NULL, 
-                             result REAL NOT NULL);''' % table_name)
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-
-def input_results():
-    connection = psycopg2.connect(user="roma",
-                                  password="thesoprano777",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="calculator_db")
-    cursor = connection.cursor()
-
-    cursor.execute(
-        '''INSERT INTO calculator (first_number, action, second_number, result) VALUES (%s,%s,%s,%s);''',
-        (our_example.first_number, our_example.action, our_example.second_number, round(our_example.calculate(), 4)))
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-
-def read_results(table_name):
-    connection = psycopg2.connect(user="roma",
-                                  password="thesoprano777",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="calculator_db")
-    cursor = connection.cursor()
-
-    cursor.execute(
-        '''SELECT * FROM %s''' % table_name)
-    table_records = cursor.fetchall()
-    if len(table_records) > 0:
-        res = []
-        for i in table_records:
-            for t in i:
-                res.append(str(t))
-            res_string = ' '.join(res)
-            print(res_string)
-            res = []
-    else:
-        print("You have no history")
-
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-
 if __name__ == "__main__":
 
+    connection = psycopg2.connect(user="roma",
+                                  password="thesoprano777",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="calculator_db")
     table_name = "calculator"
-    if not check_table(table_name):
-        create_table(table_name)
+    work_with_base = Database_work
+
+    if not work_with_base.check_table(connection, table_name):
+        work_with_base.create_table(connection, table_name)
 
     again = "y"
     while again == "y":
@@ -175,7 +108,8 @@ if __name__ == "__main__":
 
         if our_example.calculate():
             print("Your result is: ", round(our_example.calculate(), 4))
-            input_results()
+            work_with_base.input_results(connection, our_example.first_number, our_example.action,
+                                         our_example.second_number, our_example.calculate())
         else:
             print("It's not possible to divide by zero!")
 
