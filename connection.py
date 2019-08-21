83efa44c14e9e57213ab8b1dcae19efb0b191ed8
Roman Mytsko <roma.mytsko@icloud.com>
2019-08-21 21:49:25 +0300

    1)renamed my_connection.py to connection.py 2)added existence check of host and port in config.json

--- connection.py
+++ connection.py
@@ -1,4 +1,20 @@
 import psycopg2
 import json
+import sys
 
-sensetiv_data = json.dumps({"user": "roma", "password": "thesoprano777", "host": "127.0.0.1", "port": "5432", })
+with open("/Users/roma/PycharmProjects/Calculator/venv/config.json") as file:
+    config = json.load(file)
+
+if 'host' not in config.keys():
+    config['host'] = '127.0.0.1'
+if 'port' not in config.keys():
+    config['port'] = '5432'
+
+required_keys = ['user', 'password', 'host', 'port']
+try:
+    for i in required_keys:
+        if not config[i]:
+            raise Exception
+except Exception:
+    print(' "{}" has empty value.'.format(i))
+    sys.exit()
