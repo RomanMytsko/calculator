3c628bfc01f1e9a6cf6a4549fd34a0207180b2f4
Roman Mytsko <roma.mytsko@icloud.com>
2019-11-12 20:19:48 +0200

    Added run.py to run our app

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
 from app.forms import DataForm
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect
 
 
 @app.route('/')
@@ -15,13 +15,12 @@ def calculate():
     form = DataForm()
     if form.validate_on_submit():
         flash("This your's result")
-        # print(form.data['expression'])
-        # print(form.data['user_name'])
         our_expression = form.data['expression']
         user_name = form.data['user_name']
         print('Username is - ', user_name)
         print('This is our expression', our_expression)
-        return redirect(url_for('index'))
+        return redirect('/index')
     return render_template('calculate.html', title='Sign In', form=form)
 # new field for result
 # write our data to db (history) using existing functional
+
