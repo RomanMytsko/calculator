9ea59d87f0e03ef3581904dd3b06f93c0792525c
Roman Mytsko <roma.mytsko@icloud.com>
2019-11-21 14:19:46 +0200

    Aded envvar with path to json file with configs

--- connection.py
+++ connection.py
@@ -1,18 +1,10 @@
-import psycopg2
 import json
 import sys
 import os
-import my_config
 
-# CONFIG_FILE = 'config.json'
+with open(os.environ.get('CONFIG') or '/Users/roma/prog/projects/calculator/configs.json') as file:
+    config = json.load(file)
 
-# config = json.load(config.json)
-config = my_config.config
-
-# if 'host' not in config.keys():
-#     config['host'] = '127.0.0.1'
-# if 'port' not in config.keys():
-#     config['port'] = '5432'
 config['host'] = config.setdefault('host', '127.0.0.1')
 config['port'] = config.setdefault('port', '5432')
 
@@ -24,3 +16,5 @@ try:
 except Exception:
     print(' "{}" has empty value.'.format(i))
     sys.exit()
+
+
