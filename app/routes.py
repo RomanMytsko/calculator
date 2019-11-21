85136a7357fef4e1f6d8e7ae1b367ec032bfcaec
Roman Mytsko <roma.mytsko@icloud.com>
2019-11-21 11:17:53 +0200

    update

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import result as result
 from flask import render_template
 from app import app
 from app.forms import DataForm
@@ -5,7 +6,6 @@ from flask import render_template, flash, redirect
 import calculator as calc
 import models as db
 import db_connector_alchemy as session
-
 
 
 @app.route('/')
@@ -18,11 +18,9 @@ def index():
 def calculate():
     form = DataForm()
     if form.validate_on_submit():
-        flash("This your's result")
+        # flash("This your's resulttt")
         our_expression = form.data['expression']
         our_user = form.data['user_name']
-        print('this is our ex', type(our_expression))
-
         first_number, second_number, action = calc.parse_expression(our_expression)
         alchemy_action = session.AlchemyActions()
         calculator = calc.Calculator(first_number, second_number, action)
@@ -36,8 +34,11 @@ def calculate():
         to_res = db.Results(first_number, action, second_number, result, our_user_id)
         alchemy_action = session.AlchemyActions()
         alchemy_action.add_res(to_res)
-        return redirect('/index')
+        return redirect('/result')
     return render_template('calculate.html', title='Sign In', form=form)
 # new field for result
 # write our data to db (history) using existing functional
-
+@app.route('/result', result = 32)
+    # result = result
+def show_result():
+    return render_template('result.html')
--- my_config.py
+++
@@ -1,3 +0,0 @@
-config = {'user': 'roma', 'password': 'thesoprano777',
-          'host': '127.0.0.1', 'port': '5432', 'database': "calculator_db"}
-
