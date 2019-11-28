f4cf1d2d56a9d4e044029d2ba25b0c22b679806e
Roman Mytsko <roma.mytsko@icloud.com>
2019-11-28 18:05:35 +0200

    Added outputing of result

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
 
 class DataForm(FlaskForm):
     expression = StringField('Please input your expression', validators=[DataRequired()])
     user_name = StringField('Please enter your name', validators=[DataRequired()])
-    result = StringField('This is your result')
+    result = IntegerField('This is your result')
     submit = SubmitField('Calculate')
